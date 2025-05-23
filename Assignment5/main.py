6b2eccfef6936b7bb13578d95e9c1dbad040e295
Ostap Skakun <o.skakun@kse.org.ua>
2025-05-23 17:38:54 +0300

    Assignment 4 done lol

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -5,11 +5,30 @@ from pprint import pprint
 import pandas as pd
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("WEATHERAPI_KEY")
 
-BASE_URL = "https://api.openweathermap.org/data/2.5"
+BASE_URL = "https://api.weatherapi.com/v1"
 
-resp = requests.get(f"{BASE_URL}/weather?q=Kyiv&appid={API_KEY}")
+resp = requests.get(f"{BASE_URL}/forecast.json?key={API_KEY}&q=Kyiv&days=7")
 print(resp.status_code)
-pprint(resp.json())
+
+data = {
+    'temp': [],
+    'wind_spread': []
+}
+
+for day in resp.json()['forecast']['forecastday']:
+    for hour in day['hour']:
+        data['temp'].append(hour['temp_c'])
+        data['wind_spread'].append(hour['wind_kph'])
+
+df = pd.DataFrame(data)
+print(f"Max temp in 3 days: {df.loc[0:71, 'temp'].max()}")
+print(f"Min temp in 3 days: {df.loc[0:71, 'temp'].min()}")
+print(f"Avg temp in 3 days: {df.loc[0:71, 'temp'].mean()}")
+
+wind_avg = df['wind_spread'].mean()
+print(f"Avg: {wind_avg}")
+# print(df.loc[df['wind_spread'] > wind_avg])
+print(f"Number of hours wind was more than avg: {len(df.loc[df['wind_spread'] > wind_avg])}")
 
